184a05a39842a7a3e6ea94065df71fdb7caef58d
Escanor1986 <lionel.zovi@gmail.com>
2023-12-18 12:53:35 +0100

    starting fullstack portfolio project First created new express base app with express generator. Second created new angular base app and adde nglint, prettier, ngbootstrap and karma.config.ts.

---
+++ frontend/karma.conf.js
@@ -0,0 +1,40 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['Jasmine', 'AngularCli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/projet-angular'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
+        ],
+      },
+    },
+
+    singleRun: true,
+    restartOnFileChange: true,
+  });
+};
